3931976e17a1c06ee2357a236608956485f74b4a
Mert Sali <mert.sali@outlook.com>
2023-06-16 13:39:58 +0200

    This commit will add message feature and unit-Test

--- src/test/java/com/example/maintenancemonitor/maintenancemonitorControllerTest.java
+++ src/test/java/com/example/maintenancemonitor/maintenancemonitorControllerTest.java
@@ -1,7 +1,18 @@
 package com.example.maintenancemonitor;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class maintenancemonitorControllerTest {
+
+    public maintenancemonitorController maintenancemonitorController = new maintenancemonitorController();
+
+    String defaultmessage = "Everything works as expected";
+
+    @Test
+    void message(){
+        assertEquals(defaultmessage, maintenancemonitorController.message());
+    }
 
 }
